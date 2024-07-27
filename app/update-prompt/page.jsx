186304859626811a3ff9f66ec73502e3985a2b3a
Van Duy <vanduy352003@gmail.com>
2024-07-27 21:36:01 +0700

    quên import

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import {useEffect, useState} from 'react';
+import {Suspense, useEffect, useState} from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
